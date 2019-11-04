f200509b14f05148058643b3d28e0469b8546bb0
Rope <scarf666@163.com>
2019-11-04 15:37:51 +0800

    添加jsdoc接口文档

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @module 登录接口-post */
+/**
+ * @typedef {Object} 返回结果
+ * @property {number} code - 状态码
+ * @property {string} data - 返回token
+ * @property {string} msg - 信息
+ * @property {string} type - 返回类型 
+ */
+/**
+ * 小程序登录接口
+ * @param {string=} username - 用户名
+ * @param {string} password - 密码
+ */
+export function POST_USER_LOGIN() {}
